c679614a21877a87c79e7b4395ee553b11d2fa88
makquel <makquel@gmail.com>
2015-11-25 17:00:05 -0200

    inital release

--- src/airspeed_node.cpp
+++ src/airspeed_node.cpp
@@ -25,11 +25,11 @@ int main (int argc, char** argv){
     //ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
     ros::Publisher airspeed = nh.advertise<std_msgs::Float32>("airspeed_msg", 1000);
 
-    unsigned char xml_parser_on[] = { 0x7E, 0x00, 0x10, 0x17, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x30, 0xEF, 0xE6, 0xFF, 0xFE, 0x02, 0x44, 0x31, 0x05, 0x74 };
-    std::vector<unsigned char> airspeed_on(xml_parser_on, xml_parser_on+20);
+    unsigned char xml_parser_on[] = { 0x7E, 0x00, 0x10, 0x17, 0x01, 0x00, 0x7D, 0x33, 0xA2, 0x00, 0x40, 0x30, 0xEF, 0xE8, 0xFF, 0xFE, 0x02, 0x44, 0x31, 0x05, 0x72 };
+    std::vector<unsigned char> airspeed_on(xml_parser_on, xml_parser_on+21);
 
-    unsigned char xml_parser_off[] = { 0x7E, 0x00, 0x10, 0x17, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x30, 0xEF, 0xE6, 0xFF, 0xFE, 0x02, 0x44, 0x31, 0x04, 0x75 };
-    std::vector<unsigned char> airspeed_off(xml_parser_off, xml_parser_off+20);
+    unsigned char xml_parser_off[] = { 0x7E, 0x00, 0x10, 0x17, 0x01, 0x00, 0x7D, 0x33, 0xA2, 0x00, 0x40, 0x30, 0xEF, 0xE8, 0xFF, 0xFE, 0x02, 0x44, 0x31, 0x04, 0x73 };
+    std::vector<unsigned char> airspeed_off(xml_parser_off, xml_parser_off+21);
 
     try
     {
@@ -73,13 +73,13 @@ int main (int argc, char** argv){
     hexstring.push_back(0x40);
     hexstring.push_back(0x30);
     hexstring.push_back(0xEF);
-    hexstring.push_back(0xE6);
+    hexstring.push_back(0xE8);
     hexstring.push_back(0xFF);
     hexstring.push_back(0xFE);
     hexstring.push_back(0x02);
     hexstring.push_back(0x49);
     hexstring.push_back(0x53);
-    hexstring.push_back(0x52);
+    hexstring.push_back(0x50);
 
     std::vector<unsigned char> response;
     duration.sleep(); //Xbee timer after power*up
@@ -101,9 +101,10 @@ int main (int argc, char** argv){
             //std::cout << "Vector size: " << response.size() << "\n"; 
             //for(int i=0; i<response.size(); ++i)
             //    std::cout << response[i] << ' ';
-            float x = ((response[25])*256 + (response[26])*1)*3.2;
+            float x = ((response[25])*256 + (response[26])*1)*3.3;
             //std::cout << "AirSpeed_ping: [" << x << "]\n";
             result.data = x;
+            //result.data = response[25];
             ROS_INFO_STREAM("Airspeed[mV]: " << result.data);
             airspeed.publish(result);
         }
